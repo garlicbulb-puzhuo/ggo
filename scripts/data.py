8389640be202dd0c96838034823aac1731716a15
Chen Song <chens_cs@hotmail.com>
2016-09-21 20:50:09 -0400

    new code

--- scripts/data.py
+++ scripts/data.py
@@ -5,6 +5,9 @@ import os
 import logging
 import sys
 import shutil
+from img_mask_matching import get_img_mask_dict
+import re
+import json
 
 
 logger = logging.getLogger()
@@ -32,6 +35,7 @@ def get_filepaths(directory):
     tree by walking the tree either top-down or bottom-up. For each
     directory in the tree rooted at directory top (including top itself),
     it yields a 3-tuple (dirpath, dirnames, filenames).
+    :param directory:
     """
     file_paths = []  # List which will store all of the full filepaths.
 
@@ -46,55 +50,86 @@ def get_filepaths(directory):
             # Join the two strings in order to form the full filepath.
             # filepath = os.path.join(root, filename)
             # print root
-            file_paths.append(os.path.join(root, filename))  # Add it to the list.
+            # Add it to the list.
+            file_paths.append(os.path.join(root, filename))
 
     return file_paths  # Self-explanatory.
 
 
+def save(img_file, directory):
+    shutil.copy2(img_file, directory)
+
 if __name__ == '__main__':
     args = parse_options()
     # print args.input_dir
-    image_path = os.path.abspath(os.path.join(args.input_dir, 'DICOMDAT'))
-    mask_path = os.path.abspath(args.mask_input_dir)
-    file_paths = get_filepaths(image_path)
+    img_dir = os.path.abspath(os.path.join(args.input_dir, 'DICOMDAT'))
+    mask_dir = os.path.abspath(args.mask_input_dir)
+    file_paths = get_filepaths(img_dir)
+    img_mask_dict = get_img_mask_dict(img_dir, mask_dir)
 
-    logger.info("root path {0}".format(image_path))
+    logger.info("root path {0}".format(img_dir))
 
     output_path = os.path.abspath(args.output_dir)
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
-    for file_path in file_paths:
-        logger.debug("file path {0}".format(file_path))
+    for patient_id, value in img_mask_dict.items():
+        logger.info("processing patient {0}".format(patient_id))
+        img_series_dict = value['img_series']
+        patient_dir = os.path.join(output_path, patient_id)
 
-        common_prefix = os.path.commonprefix([image_path, file_path])
+        for k, v in img_series_dict.items():
+            sub_dir = re.sub(
+                '[/]', '_', re.search(r'(.*DICOMDAT/)(SDY.*)', v['path']).group(2))
+            output_image_dir = os.path.join(patient_dir, sub_dir)
+            logger.info(
+                "processing patient subdirectory {0}".format(output_image_dir))
+            if not os.path.exists(output_image_dir):
+                os.makedirs(output_image_dir)
+            for img, mask in zip(v['imgs'], v['masks']):
+                logger.debug(
+                    "processing: image -> [{0}] and mask -> {1}".format(img, mask))
+                save(img, output_image_dir)
 
-        relative_path = os.path.relpath(file_path, common_prefix)
-        logger.debug("relative path {0}".format(relative_path))
+                if mask is not None and os.path.isfile(mask):
+                    save(mask, output_image_dir)
 
-        # check and create the directory if not exists
-        parent_dir = os.path.abspath(os.path.join(file_path, os.pardir))
-        parent_relative_path = os.path.relpath(parent_dir, common_prefix)
-        output_image_dir = os.path.join(output_path, parent_relative_path)
-        if not os.path.exists(output_image_dir):
-            os.makedirs(output_image_dir)
+            logger.info(
+                "complete processing subdirectory {0}".format(output_image_dir))
 
-        mask_filepath = os.path.join(mask_path, "%s-mask.tif" % relative_path)
-        logger.debug("mask file path {0}".format(mask_filepath))
+        data = dict()
+        data['patient_id'] = patient_id
+        data['age'] = value['age']
+        data['sex'] = value['sex']
+        with open(os.path.join(patient_dir, 'data.json'), 'w') as fp:
+            json.dump(data, fp)
 
-        # copy the original image file to the output folder
-        shutil.copy2(file_path, output_image_dir)
-
-        if os.path.isfile(mask_filepath):
-            # copy the mask image file to the output folder
-            logger.info("found matching mask file {0} for {1}".format(mask_filepath, file_path))
-            shutil.copy2(mask_filepath, output_image_dir)
-        else:
-            logger.debug("no matching mask file {0} found for {0}".format(mask_filepath, file_path))
-
+    # for file_path in file_paths:
+    #     logger.debug("file path {0}".format(file_path))
+    #
+    #     common_prefix = os.path.commonprefix([image_path, file_path])
+    #
+    #     relative_path = os.path.relpath(file_path, common_prefix)
+    #     logger.debug("relative path {0}".format(relative_path))
+    #
+    #     # check and create the directory if not exists
+    #     parent_dir = os.path.abspath(os.path.join(file_path, os.pardir))
+    #     parent_relative_path = os.path.relpath(parent_dir, common_prefix)
+    #     output_image_dir = os.path.join(output_path, parent_relative_path)
+    #     if not os.path.exists(output_image_dir):
+    #         os.makedirs(output_image_dir)
+    #
+    #     mask_filepath = os.path.join(mask_path, "%s-mask.tif" % relative_path)
+    #     logger.debug("mask file path {0}".format(mask_filepath))
+    #
+    #     # copy the original image file to the output folder
+    #     shutil.copy2(file_path, output_image_dir)
+    #
+    #     if os.path.isfile(mask_filepath):
+    #         # copy the mask image file to the output folder
+    #         logger.info("found matching mask file {0} for {1}".format(mask_filepath, file_path))
+    #         shutil.copy2(mask_filepath, output_image_dir)
+    #     else:
+    #         logger.debug("no matching mask file {0} found for {0}".format(mask_filepath, file_path))
 
     # print filepaths
-
-
-
-
