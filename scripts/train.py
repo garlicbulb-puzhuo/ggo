fc1ce662b484da82d63cdc5dbf59c26635ff8872
Chen Song <chens_cs@hotmail.com>
2016-10-16 14:43:15 -0400

    more changes

--- scripts/train.py
+++ scripts/train.py
@@ -5,16 +5,26 @@ from __future__ import print_function
 from keras.models import Model
 from keras.layers import Input, merge, Convolution2D, MaxPooling2D, UpSampling2D
 from keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras.callbacks import ModelCheckpoint
 from keras import backend as K
+
+from elephas.spark_model import SparkModel
+from elephas import optimizers as elephas_optimizers
+from elephas.utils.rdd_utils import to_simple_rdd
+
+from pyspark import SparkContext, SparkConf
 
 from data_proc import train_val_data_generator, test_data_generator
 
 
-img_rows = 64
-img_cols = 64
+img_rows = 128
+img_cols = 128
 
 smooth = 100
+
+conf = SparkConf().setAppName('Spark_Backend')
+sc = SparkContext(conf=conf)
+adagrad = elephas_optimizers.Adagrad()
 
 
 def dice_coef(y_true, y_pred):
@@ -95,24 +105,81 @@ def get_unet():
     return model
 
 
+def get_spark_model(model):
+    spark_model = SparkModel(sc, model, optimizer=adagrad, frequency='epoch', mode='asynchronous', num_workers=2)
+
+    return spark_model
+
+'''
+def train_val_split(imgs, masks, index, split_ratio = 0.1): 
+    total = len(index)
+    train = []
+    val = []
+    counter = 0 
+    for i in index:
+        r = random.random()
+        if r < 0.1: 
+            val.append(counter)
+        else: 
+            train.append(counter)
+        counter += 1
+    train_imgs = imgs[train,:,:,:]
+    train_masks = masks[train,:,:,:]
+    train_index = index[train]
+    val_imgs = imgs[val,:,:,:]
+    val_masks = masks[val,:,:,:]
+    val_index = index[val]
+    return train_imgs, train_masks, train_index, val_imgs, val_masks, val_index
+'''
+
+
 def train_and_predict():
     print('-' * 30)
     print('Loading and preprocessing train data...')
     print('-' * 30)
+    #train_imgs, train_masks, train_index = load_train_data("../../../")
+
+    train_imgs_path = "/Users/csong/Developer/workspace/ggo-data/test/train_data.hdf5"
+
+    #train_imgs_p, m, st = preprocessing_imgs (train_imgs,reduced_size=(img_rows, img_cols))
+    #train_masks_p = preprocessing_masks(train_masks,reduced_size=(img_rows, img_cols))
+
+    #train_imgs, train_masks, train_index, val_imgs, val_masks, val_index = train_val_split(train_imgs_p, train_masks_p, train_index, split_ratio = 0.1)
+    print('-' * 30)
     print('Creating and compiling model...')
     print('-' * 30)
     model = get_unet()
+    spark_model = get_spark_model(model)
     model_checkpoint = ModelCheckpoint(
         'unet.hdf5', monitor='loss', save_best_only=True)
 
     print('-' * 30)
     print('Fitting model...')
     print('-' * 30)
-    nb_epoch = 2
-    data_file = '../../../train_data.hdf5'
-    for train_imgs, train_masks, train_index, val_imgs, val_masks, val_index in train_val_data_generator(data_file, train_batch_size=2, val_batch_size=1, img_rows =64, img_cols = 64, iter = 2):
-        model.fit(train_imgs, train_masks, batch_size=32, nb_epoch=nb_epoch, validation_data=(val_imgs, val_masks), verbose=1, shuffle=True, callbacks=[model_checkpoint])
 
+    nb_epoch = 5
+    verbose = 1
+
+    for e in range(nb_epoch):
+        print("epoch %d" % e)
+        for train_imgs, train_masks, train_index, val_imgs, val_masks, val_index, m, st in \
+                train_val_data_generator(train_imgs_path, train_batch_size=3, val_batch_size=1, normalization=True, reduced_size=(img_rows, img_cols)):
+            rdd = to_simple_rdd(sc, train_imgs, train_masks)
+            spark_model.train(rdd, batch_size=32, nb_epoch=nb_epoch, verbose=verbose, validation_split=0.1, num_workers=8)
+
+            # model.fit(train_imgs, train_masks, batch_size=32, nb_epoch=nb_epoch, validation_data=(
+            #     val_imgs, val_masks), verbose=verbose, shuffle=True, callbacks=[model_checkpoint])
+
+    '''
+    print('-'*30)
+    print('Loading and preprocessing test data...')
+    print('-'*30)
+    imgs_test, imgs_id_test = load_test_data()
+    imgs_test = preprocess(imgs_test)
+
+    imgs_test = imgs_test.astype('float32')
+    imgs_test -= mean
+    imgs_test /= std
 '''
     print('-' * 30)
     print('Loading and preprocessing test data...')
@@ -132,3 +199,4 @@ def train_and_predict():
 '''
 if __name__ == '__main__':
     train_and_predict()
+'''
