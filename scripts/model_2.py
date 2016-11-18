501097c13f4d1026a50016f66941858db4091862
oakcreek <zhouby98@gmail.com>
2016-11-17 20:20:13 -0500

    train multiple models

---
+++ scripts/model_2.py
@@ -0,0 +1,94 @@
+from __future__ import print_function
+
+from keras.models import Model
+from keras.layers import Input, merge, Convolution2D, MaxPooling2D, UpSampling2D, Dropout
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as K
+from train import dice_coef, dice_coef_loss
+from keras.layers.normalization import BatchNormalization
+
+
+def get_unet_BN(input_shape = (1, 128, 128), lr=1e-5, dropout_prob = 0.5):
+    inputs = Input(input_shape)
+    conv1 = Convolution2D(32, 3, 3, activation='relu',
+                          border_mode='same')(inputs)
+    BN1 = BatchNormalization()(conv1)  
+    conv1 = Convolution2D(32, 3, 3, activation='relu',
+                          border_mode='same')(BN1) 
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    dropout1 = Dropout(dropout_prob)(pool1)
+
+    conv2 = Convolution2D(64, 3, 3, activation='relu',
+                          border_mode='same')(dropout1)
+    BN2 = BatchNormalization()(conv2)
+    conv2 = Convolution2D(64, 3, 3, activation='relu',
+                          border_mode='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    dropout2 = Dropout(dropout_prob)(pool2)
+
+
+    conv3 = Convolution2D(128, 3, 3, activation='relu',
+                          border_mode='same')(dropout2)
+    BN3 = BatchNormalization()(conv3)
+    conv3 = Convolution2D(128, 3, 3, activation='relu',
+                          border_mode='same')(BN3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    dropout3 = Dropout(dropout_prob)(pool3)
+
+    conv4 = Convolution2D(256, 3, 3, activation='relu',
+                          border_mode='same')(dropout3)
+    BN4 = BatchNormalization()(conv4)
+    conv4 = Convolution2D(256, 3, 3, activation='relu',
+                          border_mode='same')(BN4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+    dropout4 = Dropout(dropout_prob)(pool4)
+
+    conv5 = Convolution2D(512, 3, 3, activation='relu',
+                          border_mode='same')(dropout4)
+    BN5 = BatchNormalization()(conv5)
+    conv5 = Convolution2D(512, 3, 3, activation='relu',
+                          border_mode='same')(BN5)
+    dropout5 = Dropout(dropout_prob)(conv5)
+
+    up6 = merge([UpSampling2D(size=(2, 2))(dropout5), conv4],
+                mode='concat', concat_axis=1)
+    conv6 = Convolution2D(256, 3, 3, activation='relu',
+                          border_mode='same')(up6)
+    BN6 = BatchNormalization()(conv6)
+    conv6 = Convolution2D(256, 3, 3, activation='relu',
+                          border_mode='same')(BN6)
+    dropout6 = Dropout(dropout_prob)(conv6)
+
+    up7 = merge([UpSampling2D(size=(2, 2))(dropout6), conv3],
+                mode='concat', concat_axis=1)
+    conv7 = Convolution2D(128, 3, 3, activation='relu',
+                          border_mode='same')(up7)
+    BN7 = BatchNormalization()(conv7)
+    conv7 = Convolution2D(128, 3, 3, activation='relu',
+                          border_mode='same')(BN7)
+    dropout7 = Dropout(dropout_prob)(conv7)
+
+    up8 = merge([UpSampling2D(size=(2, 2))(conv7), conv2],
+                mode='concat', concat_axis=1)
+    conv8 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(up8)
+    BN8 = BatchNormalization()(conv8)
+    conv8 = Convolution2D(64, 3, 3, activation='relu',
+                          border_mode='same')(BN8)
+    dropout8 = Dropout(dropout_prob)(conv8)
+
+    up9 = merge([UpSampling2D(size=(2, 2))(conv8), conv1],
+                mode='concat', concat_axis=1)
+    conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(up9)
+    BN9 = BatchNormalization()(conv9)
+    conv9 = Convolution2D(32, 3, 3, activation='relu',
+                          border_mode='same')(BN9)
+    
+    BN10 = BatchNormalization()(conv9)
+    conv10 = Convolution2D(1, 1, 1, activation='sigmoid')(BN10)
+
+    model = Model(input=inputs, output=conv10)
+
+    model.compile(optimizer=Adam(lr=lr),
+                  loss=dice_coef_loss, metrics=[dice_coef])
+    return model
--- scripts/train.py
+++ scripts/train.py
@@ -8,132 +8,47 @@ from keras.layers import Input, merge, Convolution2D, MaxPooling2D, UpSampling2D
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras import backend as K
-
 from data_proc import train_val_data_generator, test_data_generator
 
 #import theano
 #theano.config.device = 'gpu'
 #theano.config.floatX = 'float32'
 
-
+### image rows and cols
 img_rows = 64
 img_cols = 64
 
-smooth = 100
-
-
-def dice_coef(y_true, y_pred):
+def dice_coef(y_true, y_pred, smooth=100):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
-
 def dice_coef_loss(y_true, y_pred):
     return -dice_coef(y_true, y_pred)
 
-
-def get_unet():
-    inputs = Input((1, img_rows, img_cols))
-    conv1 = Convolution2D(32, 3, 3, activation='relu',
-                          border_mode='same')(inputs)
-    conv1 = Convolution2D(32, 3, 3, activation='relu',
-                          border_mode='same')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-
-    conv2 = Convolution2D(64, 3, 3, activation='relu',
-                          border_mode='same')(pool1)
-    conv2 = Convolution2D(64, 3, 3, activation='relu',
-                          border_mode='same')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-
-    conv3 = Convolution2D(128, 3, 3, activation='relu',
-                          border_mode='same')(pool2)
-    conv3 = Convolution2D(128, 3, 3, activation='relu',
-                          border_mode='same')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-
-    conv4 = Convolution2D(256, 3, 3, activation='relu',
-                          border_mode='same')(pool3)
-    conv4 = Convolution2D(256, 3, 3, activation='relu',
-                          border_mode='same')(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
-
-    conv5 = Convolution2D(512, 3, 3, activation='relu',
-                          border_mode='same')(pool4)
-    conv5 = Convolution2D(512, 3, 3, activation='relu',
-                          border_mode='same')(conv5)
-
-    up6 = merge([UpSampling2D(size=(2, 2))(conv5), conv4],
-                mode='concat', concat_axis=1)
-    conv6 = Convolution2D(256, 3, 3, activation='relu',
-                          border_mode='same')(up6)
-    conv6 = Convolution2D(256, 3, 3, activation='relu',
-                          border_mode='same')(conv6)
-
-    up7 = merge([UpSampling2D(size=(2, 2))(conv6), conv3],
-                mode='concat', concat_axis=1)
-    conv7 = Convolution2D(128, 3, 3, activation='relu',
-                          border_mode='same')(up7)
-    conv7 = Convolution2D(128, 3, 3, activation='relu',
-                          border_mode='same')(conv7)
-
-    up8 = merge([UpSampling2D(size=(2, 2))(conv7), conv2],
-                mode='concat', concat_axis=1)
-    conv8 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(up8)
-    conv8 = Convolution2D(64, 3, 3, activation='relu',
-                          border_mode='same')(conv8)
-
-    up9 = merge([UpSampling2D(size=(2, 2))(conv8), conv1],
-                mode='concat', concat_axis=1)
-    conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(up9)
-    conv9 = Convolution2D(32, 3, 3, activation='relu',
-                          border_mode='same')(conv9)
-
-    conv10 = Convolution2D(1, 1, 1, activation='sigmoid')(conv9)
-
-    model = Model(input=inputs, output=conv10)
-
-    model.compile(optimizer=Adam(lr=1e-5),
-                  loss=dice_coef_loss, metrics=[dice_coef])
-    print(model.layers)
-    return model
-
-
-def train_and_predict():
+def train_model():
     print('-' * 30)
     print('Loading and preprocessing train data...')
     print('-' * 30)
     print('Creating and compiling model...')
     print('-' * 30)
-    model = get_unet()
+    ### import model 
+    from model_2 import get_unet_BN
+    input_shape = (1, 64, 64)
+    model = get_unet_BN(input_shape)
+    train_output = 'unet123_BN.hdf5'
     model_checkpoint = ModelCheckpoint(
-        'unet.hdf5', monitor='loss', save_best_only=True)
+        train_output, monitor='loss', save_best_only=True)
 
     print('-' * 30)
     print('Fitting model...')
     print('-' * 30)
-    nb_epoch = 2
-    data_file = '../../../train_data.hdf5'
-    for train_imgs, train_masks, train_index, val_imgs, val_masks, val_index in train_val_data_generator(data_file, train_batch_size=2, val_batch_size=1, img_rows =64, img_cols = 64, iter = 2):
+    nb_epoch = 100
+    data_file = '../../../train_data_123.hdf5'
+    for train_imgs, train_masks, train_index, val_imgs, val_masks, val_index in train_val_data_generator(data_file, train_batch_size=2, val_batch_size=1, img_rows =img_rows, img_cols = img_cols, iter = 2):
         model.fit(train_imgs, train_masks, batch_size=32, nb_epoch=nb_epoch, validation_data=(val_imgs, val_masks), verbose=1, shuffle=True, callbacks=[model_checkpoint])
 
-'''
-    print('-' * 30)
-    print('Loading and preprocessing test data...')
-    print('-' * 30)
-    print('Loading saved weights...')
-    print('-' * 30)
 
-    model.load_weights('unet.hdf5')
-    print(model.layers)
-    test_file = '../../../test_data.hdf5'
-    for imgs, masks, index in test_data_generator(test_file, img_rows, img_cols, iter=2):
-        print('-' * 30)
-        print('Predicting masks on test data...')
-        print('-' * 30)
-        mask_test = model.predict(imgs, verbose=1)
-        print(mask_test)
-'''
-if __name__ == '__main__':
-    train_and_predict()
+if __name__ == "__main__": 
+    train_model()
