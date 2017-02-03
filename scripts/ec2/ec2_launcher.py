c7178c72c38a1bc960db0bf64b72bb7c35be32db
Chen Song <chen.song.82@gmail.com>
2017-02-02 21:00:15 -0500

    update ec2 launcher

--- scripts/ec2/ec2_launcher.py
+++ scripts/ec2/ec2_launcher.py
@@ -12,6 +12,7 @@ import logging
 import sys
 import ConfigParser
 import os
+import argparse
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -19,47 +20,60 @@ logger.setLevel(logging.INFO)
 logging_handler_out = logging.StreamHandler(sys.stdout)
 logger.addHandler(logging_handler_out)
 
-engine = create_engine('mysql+mysqldb://root@localhost/ggo')
 
-session = sessionmaker()
-session.configure(bind=engine)
-Base.metadata.create_all(engine)
+def get_parser():
+    parser = argparse.ArgumentParser(description='lauch task from ec2')
+    parser.add_argument('--dburl', metavar='dburl', nargs='?',
+                        help='the mysql db url')
+    return parser
 
-s = session()
-rs = s.query(Task).all()
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+    if not args.dburl:
+        parser.error('Required to set --dburl')
 
-for task in rs:
-    if task.state == 'pending':
-        # start processing the task
-        task.state = 'processing'
+    engine = create_engine(args.dburl)
 
-        working_dir = task.working_dir
+    session = sessionmaker()
+    session.configure(bind=engine)
+    Base.metadata.create_all(engine)
 
-        s.add(task)
-        s.commit()
-        break
+    s = session()
+    rs = s.query(Task).all()
 
-if working_dir is None:
-    logger.info('No pending task to be processed. Exit the program.')
-    sys.exit(0)
-else:
-    logger.info('Found one pending task in %s.' % working_dir)
+    for task in rs:
+        if task.state == 'pending':
+            # start processing the task
+            task.state = 'processing'
 
-mount_path = '~/mnt/s3'
-working_dir = os.path.join(mount_path, working_dir)
+            working_dir = task.working_dir
 
-# read config file from the working directory
-config = ConfigParser.ConfigParser()
-task_config_file = os.path.join(mount_path, 'config.ini')
-config.read(task_config_file)
-data_config = dict(config.items('config'))
+            s.add(task)
+            s.commit()
+            break
 
-# prepare for arguments
-train_imgs_path=data_config.get('train_imgs_path')
-config_file=data_config.get('config_file')
-prog_args = ['--train', '--train_imgs_path', train_imgs_path, '--train_mode', 'standalone', '--config_file', config_file]
-logger.info('Preparing for main program arguments [%s]' % ','.join(map(str, prog_args)))
+    if working_dir is None:
+        logger.info('No pending task to be processed. Exit the program.')
+        sys.exit(0)
+    else:
+        logger.info('Found one pending task in %s.' % working_dir)
 
-# launch main program
-main(prog_args)
+    mount_path = '~/mnt/s3'
+    working_dir = os.path.join(mount_path, working_dir)
+
+    # read config file from the working directory
+    config = ConfigParser.ConfigParser()
+    task_config_file = os.path.join(mount_path, 'config.ini')
+    config.read(task_config_file)
+    data_config = dict(config.items('config'))
+
+    # prepare for arguments
+    train_imgs_path=data_config.get('train_imgs_path')
+    config_file=data_config.get('config_file')
+    prog_args = ['--train', '--train_imgs_path', train_imgs_path, '--train_mode', 'standalone', '--config_file', config_file]
+    logger.info('Preparing for main program arguments [%s]' % ','.join(map(str, prog_args)))
+
+    # launch main program
+    main(prog_args)
 
