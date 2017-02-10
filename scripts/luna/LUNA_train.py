b78d19d54abb609da0bfc9050d52a3682876ef40
Chen Song <chen.song.82@gmail.com>
2017-02-09 22:18:27 -0500

    format code

--- scripts/luna/LUNA_train.py
+++ scripts/luna/LUNA_train.py
@@ -21,6 +21,7 @@ K.set_image_dim_ordering('th')  # Theano dimension ordering in this code
 
 smooth = 1.
 
+
 def dice_coef(y_true, y_pred):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
@@ -38,61 +39,49 @@ def dice_coef_np(y_true, y_pred):
 def dice_coef_loss(y_true, y_pred):
     return -dice_coef(y_true, y_pred)
 
-def data_generator(path, batch_size=2, img_rows=512, img_cols=512, shuffle=True): 
+
+def data_generator(path, batch_size=2, img_rows=512, img_cols=512, shuffle=True):
     f = []
-    f = glob(path+"*Images_*.npy")
+    f = glob(path + "*Images_*.npy")
     N = len(f)
-    n = 0 
-    while True: 
+    n = 0
+    while True:
         in_imgs = np.load(f[n])
-        in_masks = np.load(f[n].replace("Images","Masks"))
+        in_masks = np.load(f[n].replace("Images", "Masks"))
         num = in_imgs.shape[0]
         mean = np.mean(in_imgs)
         std = np.std(in_imgs)
-        in_imgs-= mean  # images should already be standardized, but just in case
+        in_imgs -= mean  # images should already be standardized, but just in case
         in_imgs /= std
-        if num>0 : 
+        if num > 0:
             if shuffle:
                 ix = np.arange(num)
                 np.random.shuffle(ix)
                 in_imgs = in_imgs[ix, :, :, :]
                 in_masks = in_masks[ix, :, :, :]
-            if (num < batch_size): 
+            if (num < batch_size):
                 out = (in_imgs, in_masks)
                 yield out
-            else: 
-                k = 0 
-                while k+batch_size <= num: 
-                    out = (in_imgs[k:(k+batch_size),:,:,:], in_masks[k:(k+batch_size),:,:,:])
+            else:
+                k = 0
+                while k + batch_size <= num:
+                    out = (in_imgs[k:(k + batch_size), :, :, :],
+                           in_masks[k:(k + batch_size), :, :, :])
                     k += batch_size
                     yield out
-                if k<num: 
-                    out = (in_imgs[k:num,:,:,:], in_masks[k:num,:,:,:])
+                if k < num:
+                    out = (in_imgs[k:num, :, :, :], in_masks[k:num, :, :, :])
                     yield out
         n += 1
-        if n>=N: 
-            n=0 
+        if n >= N:
+            n = 0
 
 
 def train_and_predict(use_existing, train_path, val_path, train_config):
     print('-' * 30)
     print('Loading and preprocessing train data...')
     print('-' * 30)
-    '''
-    imgs_train = np.load(os.path.join(
-        train_path, "trainImages.npy")).astype(np.float32)
-    imgs_mask_train = np.load(os.path.join(
-        train_path, "trainMasks.npy")).astype(np.float32)
 
-    # imgs_test = np.load(working_path+"testImages.npy").astype(np.float32)
-    # imgs_mask_test_true = np.load(working_path+"testMasks.npy").astype(np.float32)
-
-    mean = np.mean(imgs_train)  # mean for data centering
-    std = np.std(imgs_train)  # std for data normalization
-
-    imgs_train -= mean  # images should already be standardized, but just in case
-    imgs_train /= std
-    '''
     print('-' * 30)
     print('Creating and compiling model...')
     print('-' * 30)
@@ -121,7 +110,8 @@ def train_and_predict(use_existing, train_path, val_path, train_config):
 
     input_shape = (1, img_rows, img_cols)
     model, model_name = get_model(input_shape)
-    model_callbacks = get_standalone_model_callbacks(model_name=model_name, model_id=model_id, train_config=train_config)
+    model_callbacks = get_standalone_model_callbacks(
+        model_name=model_name, model_id=model_id, train_config=train_config)
 
     # Saving weights to unet.hdf5 at checkpoints
     # model_checkpoint = ModelCheckpoint('unet.hdf5', monitor='loss', save_best_only=True)
@@ -143,40 +133,16 @@ def train_and_predict(use_existing, train_path, val_path, train_config):
     print('Fitting model...')
     print('-' * 30)
 
-    #model.fit(imgs_train, imgs_mask_train, batch_size=batch_size, nb_epoch=nb_epoch,
-    #          verbose=1, shuffle=True, validation_split=validation_split, callbacks=model_callbacks)
     model.fit_generator(
-            generator=data_generator(train_path, batch_size=batch_size),
-            samples_per_epoch=2000, nb_epoch=nb_epoch, verbose=1, callbacks=model_callbacks, max_q_size = 5, nb_worker=1,
-            validation_data=data_generator(val_path, batch_size=batch_size),
-            nb_val_samples=200)
-    '''
-    # loading best weights from training session
-    print('-'*30)
-    print('Loading saved weights...')
-    print('-'*30)
-    model.load_weights('./unet.hdf5')
-
-    print('-'*30)
-    print('Predicting masks on test data...')
-    print('-'*30)
-    num_test = len(imgs_test)
-    imgs_mask_test = np.ndarray([num_test,1,512,512],dtype=np.float32)
-    for i in range(num_test):
-        imgs_mask_test[i] = model.predict([imgs_test[i:i+1]], verbose=0)[0]
-    np.save('masksTestPredicted.npy', imgs_mask_test)
-    mean = 0.0
-    for i in range(num_test):
-        mean+=dice_coef_np(imgs_mask_test_true[i,0], imgs_mask_test[i,0])
-    mean/=num_test
-    print("Mean Dice Coeff : ",mean)
-    '''
+        generator=data_generator(train_path, batch_size=batch_size),
+        samples_per_epoch=2000, nb_epoch=nb_epoch, verbose=1, callbacks=model_callbacks, max_q_size=5, nb_worker=1,
+        validation_data=data_generator(val_path, batch_size=batch_size),
+        nb_val_samples=200)
 
 
 def get_parser():
     parser = argparse.ArgumentParser(description='Train model.')
-    #parser.add_argument('--working_path', metavar='working_path', nargs='?',
-    #                    help='working directory containing numpy formatted images and masks in the training set')
+
     parser.add_argument('--train_path', metavar='train_path', nargs='?',
                         help='train data directory containing numpy formatted images and masks in the training set')
     parser.add_argument('--val_path', metavar='val_path', nargs='?',
@@ -201,6 +167,5 @@ if __name__ == '__main__':
     config.read(args.config_file)
     data_config = dict(config.items('config'))
 
-    train_and_predict(True, train_path=args.train_path, val_path=args.val_path, train_config=data_config)
-    #for i in range(100):
-    #    print(np.mean(data_generator("/Users/zhobuy98/workspace/kaggle3/output/luna16/3_slice/train/", batch_size=100).next()[0]))
+    train_and_predict(True, train_path=args.train_path,
+                      val_path=args.val_path, train_config=data_config)
